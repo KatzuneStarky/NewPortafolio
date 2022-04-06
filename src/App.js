277844e80783eb39e4c6ca91a885e38866be7ece
KatzuneStarky <aramirez_17@alu.uabcs.mx>
2022-04-06 13:06:08 -0600

    github pages

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Header />
-        <Home />
         <Routes>          
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
